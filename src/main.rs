6f65bc815661e70d92eca515d07f77f36fb26464
melo484b <melodev484b@gmail.com>
2023-02-12 22:44:19 -0500

    Move file-reading logic to run fn

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,14 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    println!("In file{}", config.file_path);
+    run(config);
+}
 
+fn run(config: Config) {
     let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
+    
+        println!("With text:]n{contents}");
 }
 
 struct Config {
